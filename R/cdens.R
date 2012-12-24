f48ef14f63a986697372ff58d29d98b0e9ecdc34
Renato Vitolo <renato.vitolo@banca.mps.it>
2012-12-24 00:00:00 +0000

    version 0.6-0

--- R/cdens.R
+++ R/cdens.R
@@ -13,12 +13,32 @@ function(distn, H)
     argp <- paste(head(names(args),length(args)-1), collapse = ", ")
     f <- function(){}
     formals(f) <- c(alist(x=), args, list(H = H))
-    body(f) <- parse(text = paste(
-        "{i <- which(x <= H);
+    if(substring(distn, 2)=="gamma"){
+      body(f) <- parse(text = "
+        {i <- which(x <= H);
+         logarg <- \"log\" %in% names(as.list(match.call()));
+         if(!missing(rate)){
+          den <- 1 - pgamma(H, shape, rate)
+          if (logarg && log)
+           res <- dgamma(x, shape, rate, log=log) - log(den)
+          else res <- dgamma(x, shape, rate, log=log)/den
+          res[i] <- 0
+         }else{
+          den <- 1 - pgamma(H, shape, scale)
+          if (logarg && log)
+          res <- dgamma(x, shape, scale, log=log) - log(den)
+         else res <- dgamma(x, shape, scale, log=log)/den
+         res[i] <- 0      
+         };
+         return(res)}")
+    }else{
+      body(f) <- parse(text = paste(
+                         "{i <- which(x <= H);
         logarg <- \"log\" %in% names(as.list(match.call()));
         den <- 1 -", pdens, "(H, ", argp, ");
         if (logarg && log) res <-", ddens, "(x, ", argd, ") - log(den)
         else res <-", ddens, "(x, ", argd, ")/den;
         res[i] <- 0; return(res)}"))
+    }
     return(f)
 }
